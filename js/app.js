0459e4a5d82c01ef476e577db96b5544bc6e8233
Nejc Vukovic <nejc.vukovic@nvwebd.com>
2023-01-08 14:51:45 +0100

    chore: implement grid, custom search, finish html structure

--- js/app.js
+++ js/app.js
@@ -2,13 +2,28 @@ window.addEventListener('load', () => {
   baguetteBox.run('.gallery');
 });
 
-const searchBoxHandler = (event) => {
-  console.log('event.target.value: ', event.target.value);
-}
+const handleSearch = (event, nodeList) => {
+  const searchValue = event.target.value;
+  
+  nodeList.forEach(node => {
+    let nodeDataCaption = node.getAttribute('data-caption').toLowerCase();
+    
+    if (nodeDataCaption.includes(searchValue)) {
+      node.style.display = "";
+    } else {
+      node.style.display = 'none';
+    }
+  });
+};
 
 const app = () => {
-  const searchBox = document.querySelector('#search-box');
-  searchBox.addEventListener('input', searchBoxHandler);
-}
+  const links = document.querySelectorAll('a');
+  const searchBox = document.querySelector('#search');
+  
+  searchBox.addEventListener('keyup', (event) => {
+    handleSearch(event, links);
+  });
+};
 
 app();
+
